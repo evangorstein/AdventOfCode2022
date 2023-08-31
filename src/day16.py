8fe7ff1f98d199b2d6ec4b95fc7216ed457d16ae
Evan Gorstein <egorstein@wisc.edu>
2023-08-31 14:41:25 -0400

    Day 16 part 1 completed

--- src/day16.py
+++ src/day16.py
@@ -1,18 +1,19 @@
+# Solution shamelessly pieced together from https://pastebin.com/u4crb1Fm
+# and https://github.com/mebeim/aoc/blob/master/2022/solutions/day16.py
+
 import sys
-import pathlib
+import time
 from dataclasses import dataclass
 
 INF = int(1e9)
-INPUT_PATH = sys.argv[1] if len(sys.argv) > 1 else "..data/ex_day16.txt"
+INPUT_PATH = sys.argv[1] if len(sys.argv) > 1 else "../data/ex_day16.txt"
 
 @dataclass
 class Valve:
-    name : str
     flow_rate : int
     children : list[str]
 
-
-def parse_input(puzzle_input):
+def parse_input(puzzle_input: list[str]) -> dict[str, Valve]:
 
     valves = {}
 
@@ -21,12 +22,16 @@ def parse_input(puzzle_input):
         name = split[1]
         flow_rate = int(split[4].split("=")[1][:-1])
         children = [split[-1]] + [token[:-1] for token in split if token.endswith(",")]
-        valves[name] = Valve(name, flow_rate, children)
+        valves[name] = Valve(flow_rate, children)
 
     return(valves)
 
+def floid_warshall(valves: dict[str, Valve]) -> dict[str, dict[str, int]]:
+    """
+    Returns valve-valve distance matrix in the form of a dictionary of dictionaries. 
+    Uses floid_warshall algorithm to compute shortest paths.
+    """
 
-def floid_warshall(valves):
     dist = {v: {u: INF for u in valves} for v in valves}
  
     for v in valves:
@@ -41,17 +46,48 @@ def floid_warshall(valves):
  
     return dist
 
+def gen_options(dist: dict[str, dict[str, int]], all_valve_names: set[str], 
+                cur: str, opened: dict[str, int], time: int):
+    """
+    Iterator over all possible options.
+    Doing DFS over a complete graph.
+    Each option is a dictionary whose keys are the names of all valves opened for that option. 
+    The values are the time remaining when that valve was opened.
+    For example, an option could be {"AA" : 30, "BB" : 15}
+    This means that "AA" was opened with 30 minutes to go and "BB" was opened with 15 minutes to go
+    """
+    yield opened 
 
+    for nxt in all_valve_names - opened.keys(): # iterate through all unopened valves
+        # get time remaining after traveling to nxt and opening it
+        new_time = time - dist[cur][nxt] - 1 
+        if new_time <= 0: # no time to open nxt
+            continue
+
+        new_cur = nxt
+        new_opened = opened.copy()
+        new_opened[nxt] = new_time 
+        yield from gen_options(dist, all_valve_names, new_cur, new_opened, new_time)
+
+def get_score(option: dict[str, int], valves: dict[str, Valve]) -> int:
+    return(sum([option[v]*valves[v].flow_rate for v in option]))
 
 if __name__ == "__main__":  
-    print(f"\nData is from {INPUT_PATH}")
+    print(f"Data is from {INPUT_PATH}")
 
     with open(INPUT_PATH) as f:
         puzzle_input = f.readlines()
     
     valves = parse_input(puzzle_input)
     dist = floid_warshall(valves)
-    print(dist)
+    nz_valve_names = set([v for v in valves if valves[v].flow_rate > 0])
+    all_options = gen_options(dist, nz_valve_names, "AA", {}, 30)
+    best = max(get_score(option, valves) for option in all_options)
+    print(best)
+
+
+
+
     
      
 
